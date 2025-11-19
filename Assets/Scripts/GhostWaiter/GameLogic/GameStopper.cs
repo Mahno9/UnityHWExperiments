911364c992e4c5f4ce18bb8f82b738d34bf69d92
Mahno9 <mahno32@mail.ru>
2025-11-19 10:04:46 +0500

    Win/Lose endings

---
+++ Assets/Scripts/GhostWaiter/GameLogic/GameStopper.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class GameStopper : MonoBehaviour
+{
+    [SerializeField] private BaseValueContainer _gameState;
+
+    private void Update()
+    {
+        if (_gameState.GetValue() < 0 || _gameState.GetValue() >= _gameState.GetValueMax())
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/GhostWaiter/UI/WinLoseShower.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class WinLoseShower : MonoBehaviour
+{
+    [SerializeField] private BaseValueContainer _gameState;
+
+    [SerializeField] private RectTransform _winContainer;
+    [SerializeField] private RectTransform _loseContainer;
+
+    private void Update()
+    {
+        if (_gameState.GetValue() < 0)
+        {
+            _loseContainer.gameObject.SetActive(true);
+            enabled = false;
+        }
+        else if (_gameState.GetValue() >= _gameState.GetValueMax())
+        {
+            _winContainer.gameObject.SetActive(true);
+            enabled = false;
+        }
+    }
+}
