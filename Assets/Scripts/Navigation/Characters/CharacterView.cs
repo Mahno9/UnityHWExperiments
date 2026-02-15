d012edf54cb9cef9663f2174b3d58897f15edae9
Mahno9 <mahno32@mail.ru>
2026-02-15 17:14:09 +0500

    Sounds and music

--- Assets/Scripts/Navigation/Characters/CharacterView.cs
+++ Assets/Scripts/Navigation/Characters/CharacterView.cs
@@ -13,13 +13,17 @@ namespace Navigation.Characters
         private const string InjuredLayerName = "Injured";
         private const float  MaxLayerWeight   = 1;
 
-        [SerializeField] private Animator _animator;
-        [SerializeField] private float    _injureHealth       = 30;
-        private readonly         int      _damagedKey         = Animator.StringToHash("Damaged");
-        private readonly         int      _isDeadKey          = Animator.StringToHash("IsDead");
-        private readonly         int      _isInJumpProcessKey = Animator.StringToHash("IsInJumpProcess");
+        [SerializeField] private Animator    _animator;
+        [SerializeField] private float       _injureHealth = 30;
+        [SerializeField] private AudioSource _jumpStartSFX;
+        [SerializeField] private AudioSource _jumpFinishSFX;
+        private readonly         int         _damagedKey         = Animator.StringToHash("Damaged");
+        private readonly         int         _isDeadKey          = Animator.StringToHash("IsDead");
+        private readonly         int         _isInJumpProcessKey = Animator.StringToHash("IsInJumpProcess");
 
         private readonly int _isRunningKey = Animator.StringToHash("IsRunning");
+
+        private bool _prevIsInJumpProgress = false;
 
         private Character _character;
 
@@ -46,7 +50,20 @@ namespace Navigation.Characters
 
         private void UpdateJumping()
         {
-            _animator.SetBool(_isInJumpProcessKey, _character.IsInJumpProcess);
+            bool isJumping = _character.IsInJumpProcess;
+            ProcessJumpSFX(isJumping);
+            _animator.SetBool(_isInJumpProcessKey, isJumping);
+        }
+
+        private void ProcessJumpSFX(bool isJumping)
+        {
+            if (_prevIsInJumpProgress != isJumping)
+            {
+                if (isJumping) _jumpStartSFX.Play();
+                else _jumpFinishSFX.Play();
+            }
+
+            _prevIsInJumpProgress = isJumping;
         }
 
         private void UpdateIsRunning()
