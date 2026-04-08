782197e521449cbe4399ceaa8caada0e6e5158ed
Mahno9 <mahno32@mail.ru>
2026-04-08 22:15:19 +0500

    Inline DamageableCharacter into EnemyCharacter and MainCharacter, remove base class

--- Assets/Scripts/MiniGame/Characters/EnemyCharacter.cs
+++ Assets/Scripts/MiniGame/Characters/EnemyCharacter.cs
@@ -1,26 +1,36 @@
+using System;
+
 using Delegates.Enemies.Controllers;
 
 using Navigation.Characters.Interfaces;
 using Navigation.Controllers;
 using Navigation.CoreMechanics.Movement;
 using Navigation.CoreMechanics.Rotation;
+using Navigation.Utils;
 
 using UnityEngine;
 
 namespace MiniGame.Characters
 {
-    public class EnemyCharacter : DamageableCharacter, IMovable
+    public class EnemyCharacter : ControllersUpdater, IMovable, IDamageable, IDying
     {
+        public IReactiveVariableReadonly<float> Health => _health;
+        public IReactiveVariableReadonly<bool>  IsDead => _isDead;
+
         public float   MoveSpeed => _mover.MoveSpeed;
         public Vector3 Position  => _mover.Position;
 
+        private readonly ReactiveVariable<float>    _health;
+        private readonly ReactiveVariable<bool>     _isDead;
         private readonly AlongMoverDirectionRotator _rotator;
         private readonly NavMeshAgentMover          _mover;
 
-        public EnemyCharacter(NavMeshAgentMover mover, AlongMoverDirectionRotator rotator, float startHealth, params ControllerBase[] controllers) : base(startHealth, controllers)
+        public EnemyCharacter(NavMeshAgentMover mover, AlongMoverDirectionRotator rotator, float startHealth, params ControllerBase[] controllers) : base(controllers)
         {
-            _mover = mover;
+            _mover   = mover;
             _rotator = rotator;
+            _health  = new ReactiveVariable<float>(startHealth);
+            _isDead  = new ReactiveVariable<bool>(false);
         }
 
         public override void Update(float deltaTime)
@@ -28,6 +38,15 @@ namespace MiniGame.Characters
             base.Update(deltaTime);
 
             _rotator.Update(deltaTime);
+        }
+
+        public virtual void TakeDamage(float damage)
+        {
+            if (_isDead.Value) return;
+
+            _health.Value = MathF.Max(0, _health.Value - damage);
+            if (_health.Value <= 0)
+                _isDead.Value = true;
         }
 
         public void SetMovePoint(Vector3 point) => _mover.SetMovePoint(point);
--- Assets/Scripts/MiniGame/Characters/MainCharacter.cs
+++ Assets/Scripts/MiniGame/Characters/MainCharacter.cs
@@ -1,21 +1,33 @@
+using System;
+
+using Navigation.Characters.Interfaces;
 using Navigation.Controllers;
 using Navigation.CoreMechanics.Rotation;
+using Navigation.Utils;
+
 using UnityEngine;
 
 namespace MiniGame.Characters
 {
-    public class MainCharacter : DamageableCharacter, ISimpleMovable, IRotatableInPosition
+    public class MainCharacter : ControllersUpdater, ISimpleMovable, IRotatableInPosition, IDamageable, IDying
     {
+        public IReactiveVariableReadonly<float> Health => _health;
+        public IReactiveVariableReadonly<bool>  IsDead => _isDead;
+
         public Vector3 Position      => _characterController.transform.position;
         public float   RotationSpeed => _rotator.RotationSpeed;
 
-        private readonly CharacterController _characterController;
-        private readonly DirectionRotator    _rotator;
+        private readonly ReactiveVariable<float> _health;
+        private readonly ReactiveVariable<bool>  _isDead;
+        private readonly CharacterController     _characterController;
+        private readonly DirectionRotator        _rotator;
 
-        public MainCharacter(CharacterController characterController, DirectionRotator rotator, float health, params ControllerBase[] controllers) : base(health, controllers)
+        public MainCharacter(CharacterController characterController, DirectionRotator rotator, float health, params ControllerBase[] controllers) : base(controllers)
         {
             _characterController = characterController;
-            _rotator = rotator;
+            _rotator             = rotator;
+            _health              = new ReactiveVariable<float>(health);
+            _isDead              = new ReactiveVariable<bool>(false);
         }
 
         public override void Update(float deltaTime)
@@ -24,6 +36,15 @@ namespace MiniGame.Characters
 
             // _characterController updates just after move
             _rotator.Update(deltaTime);
+        }
+
+        public virtual void TakeDamage(float damage)
+        {
+            if (_isDead.Value) return;
+
+            _health.Value = MathF.Max(0, _health.Value - damage);
+            if (_health.Value <= 0)
+                _isDead.Value = true;
         }
 
         public void Move(Vector3 direction) => _characterController.Move(direction);
